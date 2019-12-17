19f180340417fcab7d50a23cf07f61abe7b9f03e
Victoria Curl <victoriacurl9@gmail.com>
2019-12-17 23:20:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,8 +29,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
-
+array = [2,3,"am"]
+array [2]
 end
 
 def retrieve_first_element_from_array(array)
