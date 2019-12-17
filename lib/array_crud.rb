ecfffc3a23efbf3467669aedbd3c8ff3c2a5db8d
Victoria Curl <victoriacurl9@gmail.com>
2019-12-17 23:18:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,10 +23,13 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  array = [1, 2, 3, 4]
+  array.unshift("wow")
+  wow = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+
 
 end
 
