ab0dcd14de8cb0385f51cdec050a0c5ff1274093
Victoria Curl <victoriacurl9@gmail.com>
2019-12-17 23:07:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array = [1, 2, 3, 4] << 5
+array = [1, 2, 3, 4] << "arrays"
 end
 
 def add_element_to_start_of_array(array, element)
